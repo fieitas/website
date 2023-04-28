3d93d0d1671c1b2462ae3e4e793310db4d3b95b7
bott <bott@adobe.com>
2023-04-28 12:04:52 +0200

    fix linting ?

--- scripts/dom-helper.js
+++ scripts/dom-helper.js
@@ -28,31 +28,31 @@
  * @returns {Element} The constructred DOM Element
  */
 export function domEl(tag, ...items) {
-    const element = document.createElement(tag);
+  const element = document.createElement(tag);
 
-    if (!items || items.length === 0) return element;
+  if (!items || items.length === 0) return element;
 
-    if (!(items[0] instanceof Element || items[0] instanceof HTMLElement) && typeof items[0] === 'object') {
-        const [attributes, ...rest] = items;
-        items = rest;
+  if (!(items[0] instanceof Element || items[0] instanceof HTMLElement) && typeof items[0] === 'object') {
+    const [attributes, ...rest] = items;
+    items = rest;
 
-        Object.entries(attributes).forEach(([key, value]) => {
-            if (!key.startsWith('on')) {
-                element.setAttribute(key, Array.isArray(value) ? value.join(' ') : value);
-            } else {
-                element.addEventListener(key.substring(2).toLowerCase(), value);
-            }
-        });
-    }
-
-    items.forEach((item) => {
-        item = item instanceof Element || item instanceof HTMLElement
-            ? item
-            : document.createTextNode(item);
-        element.appendChild(item);
+    Object.entries(attributes).forEach(([key, value]) => {
+      if (!key.startsWith('on')) {
+        element.setAttribute(key, Array.isArray(value) ? value.join(' ') : value);
+      } else {
+        element.addEventListener(key.substring(2).toLowerCase(), value);
+      }
     });
+  }
 
-    return element;
+  items.forEach((item) => {
+    item = item instanceof Element || item instanceof HTMLElement
+      ? item
+      : document.createTextNode(item);
+    element.appendChild(item);
+  });
+
+  return element;
 }
 
 /*
