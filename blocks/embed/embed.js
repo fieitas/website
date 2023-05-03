144221b960b74504ab324c20b59aedec0ad060c6
Alejandro Ramirez <ramirezc@adobe.com>
2023-05-03 09:36:44 +0200

    embed from block collection

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -1,26 +1,31 @@
-import { toClassName } from '../../scripts/lib-franklin.js';
-import { customDecorateIcons } from '../../scripts/scripts.js';
+/*
+ * Embed Block
+ * Show videos and social posts directly on your page
+ * https://www.hlx.live/developer/block-collection/embed
+ */
 
-const getDefaultEmbed = (
-  url,
-) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+const loadScript = (url, callback, type) => {
+  const head = document.querySelector('head');
+  const script = document.createElement('script');
+  script.src = url;
+  if (type) {
+    script.setAttribute('type', type);
+  }
+  script.onload = callback;
+  head.append(script);
+  return script;
+};
+
+const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
     <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
       scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
     </iframe>
   </div>`;
 
-const embedYumpu = (
-  url,
-) => `<div class="embed-container" id="ypembedcontainer" style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-  <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
-    scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
-  </iframe>
-</div>`;
-
 const embedYoutube = (url, autoplay) => {
   const usp = new URLSearchParams(url.search);
   const suffix = autoplay ? '&muted=1&autoplay=1' : '';
-  let vid = usp.get('v') && encodeURIComponent(usp.get('v'));
+  let vid = encodeURIComponent(usp.get('v'));
   const embed = url.pathname;
   if (url.origin.includes('youtu.be')) {
     [, vid] = url.pathname.split('/');
@@ -32,85 +37,53 @@ const embedYoutube = (url, autoplay) => {
   return embedHTML;
 };
 
-const embedYoutubeChannel = async (url) => {
-  // offload the Youtube Channel embed as this is only used on the social media page
-  const mod = await import('./embed-youtube-channel.js');
-  let embedHTML = '';
-  if (mod.default) {
-    embedHTML = await mod.default(url);
-  }
+const embedVimeo = (url, autoplay) => {
+  const [, video] = url.pathname.split('/');
+  const suffix = autoplay ? '?muted=1&autoplay=1' : '';
+  const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+      <iframe src="https://player.vimeo.com/video/${video}${suffix}" 
+      style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+      frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  
+      title="Content from Vimeo" loading="lazy"></iframe>
+    </div>`;
   return embedHTML;
 };
 
-const embedInstagram = async (url) => {
-  // offload the instagram template to only load the large template if really needed
-  const mod = await import('./embed-instagram.js');
-  let embedHTML = '';
-  if (mod.default) {
-    embedHTML = await mod.default(url);
-  }
+const embedTwitter = (url) => {
+  const embedHTML = `<blockquote class="twitter-tweet"><a href="${url.href}"></a></blockquote>`;
+  loadScript('https://platform.twitter.com/widgets.js');
   return embedHTML;
 };
 
-const embedFacebook = async (url) => {
-  // offload the Facebook Page template as this is only used on the social media page
-  const mod = await import('./embed-facebook.js');
-  let embedHTML = '';
-  if (mod.default) {
-    embedHTML = await mod.default(url);
-  }
-  return embedHTML;
-};
-
-const embedVideo = (url) => `
-  <figure class="video"><div class="video-embed">
-  <video  controls="" autoplay="" class="video-embed-item"><source src="${url}" type="video/mp4"></video>
-  </div></figure>`;
-
-const loadEmbed = async (block, link, autoplay) => {
+const loadEmbed = (block, link, autoplay) => {
   if (block.classList.contains('embed-is-loaded')) {
     return;
   }
 
   const EMBEDS_CONFIG = [
     {
-      match: ['youtube.com/user/', 'youtube.com/channel/'],
-      embed: embedYoutubeChannel,
-    },
-    {
       match: ['youtube', 'youtu.be'],
       embed: embedYoutube,
     },
     {
-      match: ['yumpu'],
-      embed: embedYumpu,
+      match: ['vimeo'],
+      embed: embedVimeo,
     },
     {
-      match: ['instagram'],
-      embed: embedInstagram,
-    },
-    {
-      match: ['facebook'],
-      embed: embedFacebook,
-    },
-    {
-      match: ['.mp4'],
-      embed: embedVideo,
+      match: ['twitter'],
+      embed: embedTwitter,
     },
   ];
 
   const config = EMBEDS_CONFIG.find((e) => e.match.some((match) => link.includes(match)));
   const url = new URL(link);
   if (config) {
-    const className = toClassName(config.match[0]);
-    block.innerHTML = await config.embed(url, autoplay);
-    block.classList = `block embed embed-${className}`;
-    customDecorateIcons(block);
+    block.innerHTML = config.embed(url, autoplay);
+    block.classList = `block embed embed-${config.match[0]}`;
   } else {
     block.innerHTML = getDefaultEmbed(url);
     block.classList = 'block embed';
   }
-
   block.classList.add('embed-is-loaded');
 };
 
