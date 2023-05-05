0967aea88bed369eb66a0b78a104794bbba001e2
Alejandro Ramirez <ramirezc@adobe.com>
2023-05-05 12:05:38 +0200

    close button + divider

--- blocks/covid-19-accordion/covid-19-accordion.js
+++ blocks/covid-19-accordion/covid-19-accordion.js
@@ -1,33 +1,10 @@
-export default async function decorate(block) {
-  // locate main section
-  const firstDivAfterAccordion = block.querySelector('.covid-19-accordion > div:first-child > div');
-
-  // locate remaining sections
-  const remainingDivsAfterAccordion = block.querySelectorAll('.covid-19-accordion > div:not(:first-child) > div');
-
-  // Extract image
-  const pictureElement = firstDivAfterAccordion.querySelector('picture');
-
-  // Extract all content up to the buttons
-  const elementsAfterPicture = Array.from(firstDivAfterAccordion.children).slice(1);
-  const firstLinkIndex = elementsAfterPicture.findIndex((element) => element.className === 'button-container');
-  const elementsBeforeFirstLink = elementsAfterPicture.slice(0, firstLinkIndex);
-
-  let extractedInnerHTML = '';
-  elementsBeforeFirstLink.forEach((element) => {
-    extractedInnerHTML += element.outerHTML;
-  });
-
-  // Extract all links
-  const linksArray = firstDivAfterAccordion.querySelectorAll('a');
-
-  // Remove original source elements
-  firstDivAfterAccordion.innerHTML = '';
-
-  // Create new structure and append extracted content
-  const container = document.createElement('div');
-  container.classList.add('container');
-
+/**
+ * Creates a banner with the extracted leading text and the picture element
+ * @param pictureElement
+ * @param extractedInnerHTML
+ * @returns {HTMLDivElement}
+ */
+function createBanner(pictureElement, extractedInnerHTML) {
   const banner = document.createElement('div');
   banner.classList.add('banner');
 
@@ -41,35 +18,90 @@ export default async function decorate(block) {
   icon.innerHTML = pictureElement.outerHTML;
   banner.appendChild(icon);
 
+  return banner;
+}
+
+/**
+ * Creates a div with the extracted links from the first row of document
+ * @param linksArray
+ * @returns {HTMLDivElement}
+ */
+function createLinks(linksArray) {
   const links = document.createElement('div');
   links.classList.add('links');
 
-  // Add extracted links as buttons
   linksArray.forEach((link, index) => {
     const button = document.createElement('button');
     button.textContent = link.textContent;
-    // first link controls the accordion button
-    // eslint-disable-next-line no-return-assign
+
     if (index === 0) {
-      button.setAttribute('id', 'expand-accordion');
-      button.setAttribute('aria-expanded', 'false');
-      button.setAttribute('aria-controls', 'details');
-      button.addEventListener('click', () => {
-        const accordion = document.querySelector('.covid-19-accordion .container .details');
-        accordion.classList.toggle('expanded');
-        if (accordion.classList.contains('expanded')) {
-          button.setAttribute('aria-expanded', 'true');
-        } else {
-          button.setAttribute('aria-expanded', 'false');
-        }
-      });
+      setupFirstButton(button);
     } else {
       // eslint-disable-next-line no-return-assign
-      button.onclick = () => window.location.href = link.href;
+      button.onclick = () => (window.location.href = link.href);
     }
+
     links.appendChild(button);
   });
 
+  return links;
+}
+
+/**
+ * Sets up the first button to expand the accordion and adds a close button to the accordion
+ * @param button
+ */
+function setupFirstButton(button) {
+  button.setAttribute('id', 'expand-accordion');
+  button.setAttribute('aria-expanded', 'false');
+  button.setAttribute('aria-controls', 'details');
+  button.addEventListener('click', () => {
+    const accordion = document.querySelector('.covid-19-accordion .container .details');
+    const existingDivider = accordion.querySelector('.custom-divider');
+
+    if (!existingDivider) {
+      const newDiv = createDividerWithCloseButton();
+      accordion.prepend(newDiv);
+    }
+
+    accordion.classList.toggle('expanded');
+    if (accordion.classList.contains('expanded')) {
+      button.setAttribute('aria-expanded', 'true');
+    } else {
+      button.setAttribute('aria-expanded', 'false');
+    }
+  });
+}
+
+/**
+ * Creates dividers with close buttons for the expanded content of accordion
+ * @returns {HTMLDivElement}
+ */
+function createDividerWithCloseButton() {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('custom-divider');
+
+  const hr = document.createElement('hr');
+  newDiv.appendChild(hr);
+
+  const closeButton = document.createElement('button');
+  closeButton.classList.add('close-button');
+  // empty as we fill in from after pseudo element
+  closeButton.innerHTML = '';
+  closeButton.addEventListener('click', () => {
+    const accordion = document.querySelector('.covid-19-accordion .container .details');
+    const button = document.querySelector('#expand-accordion');
+
+    accordion.classList.remove('expanded');
+    button.setAttribute('aria-expanded', 'false');
+  });
+  newDiv.appendChild(closeButton);
+
+  return newDiv;
+}
+
+
+function createDetails(remainingDivsAfterAccordion) {
   const details = document.createElement('div');
   details.classList.add('details');
   details.setAttribute('aria-labelledby', 'expand-accordion');
@@ -77,13 +109,32 @@ export default async function decorate(block) {
     details.appendChild(div);
   });
 
-  // clean original source elements
-  remainingDivsAfterAccordion.innerHTML = '';
+  return details;
+}
 
-  container.appendChild(banner);
-  container.appendChild(links);
-  container.appendChild(details);
+export default async function decorate(block) {
+  const firstDivAfterAccordion = block.querySelector('.covid-19-accordion > div:first-child > div');
+  const remainingDivsAfterAccordion = block.querySelectorAll('.covid-19-accordion > div:not(:first-child) > div');
+  const pictureElement = firstDivAfterAccordion.querySelector('picture');
 
-  // Replace original source elements with new structure
+  const elementsAfterPicture = Array.from(firstDivAfterAccordion.children).slice(1);
+  const firstLinkIndex = elementsAfterPicture.findIndex((element) => element.className === 'button-container');
+  const elementsBeforeFirstLink = elementsAfterPicture.slice(0, firstLinkIndex);
+
+  let extractedInnerHTML = '';
+  elementsBeforeFirstLink.forEach((element) => {
+    extractedInnerHTML += element.outerHTML;
+  });
+
+  const linksArray = firstDivAfterAccordion.querySelectorAll('a');
+  firstDivAfterAccordion.innerHTML = '';
+
+  const container = document.createElement('div');
+  container.classList.add('container');
+
+  container.appendChild(createBanner(pictureElement, extractedInnerHTML));
+  container.appendChild(createLinks(linksArray));
+  container.appendChild(createDetails(remainingDivsAfterAccordion));
+
   firstDivAfterAccordion.appendChild(container);
 }
