510054f64fd8b1a4bff796eaa55668485b782281
Andres Bott <bott@adobe.com>
2023-05-09 10:40:51 +0200

    Add Carousel Block (#12)

---
+++ blocks/carousel/carousel.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-expressions */
+import { createCarousel } from './carousel.base.js';
+
+export default async function decorate(block) {
+  const cfg = {
+    navButtons: true,
+    dotButtons: true,
+    infiniteScroll: true,
+    autoScroll: false,
+  };
+  await createCarousel(block, cfg);
+}
