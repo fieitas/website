8c8e578ba8e21f2cdd43c2b826b7f7c662c4fe08
Alejandro Ramirez <ramirezc@adobe.com>
2023-05-11 09:39:46 +0200

    add more networks

--- blocks/social-media-share/social-media-share.js
+++ blocks/social-media-share/social-media-share.js
@@ -1,4 +1,5 @@
-import { getMetadata, toCamelCase } from '../../scripts/lib-franklin.js';
+import { decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
+import { a, div, span } from '../../scripts/dom-helpers.js';
 
 const meta = {
   title: encodeURIComponent(document.title),
@@ -13,14 +14,14 @@ const socialDetails = function (networkName) {
       return {
         type: 'mail',
         label: 'mail',
-        href: `mailto:?subject=${meta.title}&amp;body=${meta.description}%0D%0A%0D%0A${meta.url}`,
+        href: `mailto:?subject=${meta.title}&body=${meta.description} ${meta.url}`,
         icon: 'mail',
       };
     case 'twitter':
       return {
         type: 'twitter',
         label: 'twitter',
-        href: `https://twitter.com/intent/tweet?url=${meta.url}&amp;text=${meta.title}`,
+        href: `https://twitter.com/intent/tweet?url=${meta.url}&text=${meta.title}`,
         icon: 'twitter',
       };
     case 'facebook':
@@ -30,24 +31,39 @@ const socialDetails = function (networkName) {
         href: `https://www.facebook.com/sharer/sharer.php?u=${meta.url}`,
         icon: 'facebook',
       };
+    case 'whatsapp':
+      return {
+        type: 'whatsapp',
+        label: 'whatsapp',
+        href: `https://wa.me/?text=${meta.title}%20${meta.url}`,
+        icon: 'whatsapp',
+      };
+    case 'telegram':
+      return {
+        type: 'telegram',
+        label: 'telegram',
+        href: `https://t.me/share/url?url=${meta.url}&text=${meta.title}`,
+        icon: 'telegram',
+      };
   }
 };
 
-// function buildMarkup(element, placeholders) {
-//   element.classList.add('share');
-//   element.classList.add('general-article-stage__share');
-//   socials(placeholders).forEach((social) => {
-//     const a = document.createElement('a');
-//     a.setAttribute('data-type', social.type);
-//     a.setAttribute('aria-label', social.label);
-//     a.setAttribute('class', 'plain-link share__link');
-//     a.setAttribute('target', '_blank');
-//     a.setAttribute('href', social.href);
-//     a.setAttribute('title', social.label);
-//     a.innerHTML = `<span class="icon icon-${social.icon}"></span>`;
-//     element.appendChild(a);
-//   });
-// }
+function buildShortened(block, socials) {
+  const allLinks = [];
+  socials.forEach((social) => {
+    const currentLink = a({
+      class: 'plain-link share__link',
+      href: social.href,
+      target: '_blank',
+      title: social.label,
+      'data-type': social.type,
+      'aria-label': social.label,
+    },
+    span({ class: `icon icon-${social.icon}` }));
+    allLinks.push(currentLink);
+  });
+  return allLinks;
+}
 
 export default async function decorate(block) {
   const socials = [];
@@ -55,5 +71,7 @@ export default async function decorate(block) {
     const network = element.textContent.toLowerCase();
     socials.push(socialDetails(network));
   });
-  console.log(socials);
+  block.innerHTML = '';
+  block.append(...buildShortened(block, socials));
+  await decorateIcons(block);
 }
