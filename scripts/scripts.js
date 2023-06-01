adf0d838fe7349de58fbf05b7ca4960bef680457
Alejandro Ramirez <ramirezc@adobe.com>
2023-06-01 10:57:57 +0200

    remove gallery LCP

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,7 +16,7 @@ import {
   waitForLCP,
 } from './lib-franklin.js';
 
-const LCP_BLOCKS = ['gallery']; // add your LCP blocks to the list
+const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 /**
  * Builds hero block and prepends to main in a new section.
